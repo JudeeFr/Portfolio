d729cf8498dad8ff74e048fae2565580ed359b1f
Judee ジ <115565273+JudeeFr@users.noreply.github.com>
2023-08-26 18:57:47 +0200

    collapse menu

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './style.css';
-import logo from '../../assets/etc/logo.png'
+import logo from '../../assets/etc/logo.png';
+import { Link } from 'react-router-dom'
+import Menu from '../../components/menu'
 
 export default function Header() {
 	return (
 		<div className='header'>
             <div className='logo'>
-			<img src={logo} alt="logo"></img>
+                <Link to="/">
+			        <img src={logo} alt="logo"></img>
+                </Link>
             </div>
             <div className='header-btns'>
-                <button className='hire-btn'>Hire me</button>
-                <button className='menu-btn'>Menu</button>
+                <Link to="/contact">
+                    <button className='hire-btn'>Hire me</button>
+                </Link>
+                <Menu/>
             </div>
 		</div>
 	);
