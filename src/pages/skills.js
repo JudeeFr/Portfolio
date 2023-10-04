1f0368f77d58d55c3fa3a958ebdfc87d5e1e4e4a
Judee ジ <115565273+JudeeFr@users.noreply.github.com>
2023-10-04 16:54:55 +0200

    skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,6 +3,24 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import tiger from '../assets/img/tiger.svg';
 import html from '../assets/icons/html-50.svg'
+import css from '../assets/icons/css3-50.svg'
+import js from '../assets/icons/javascript-50.svg'
+import react from '../assets/icons/react-50.svg'
+import redux from '../assets/icons/redux-50.svg'
+import sass from '../assets/icons/sass-50.svg'
+import wordpress from '../assets/icons/wordpress-50.svg'
+import seo from '../assets/icons/seo-50.svg'
+import git from '../assets/icons/github-50.svg'
+import vscode from '../assets/icons/visual-studio-code-50.svg'
+import firebase from '../assets/icons/firebase-50.svg'
+import xd from '../assets/icons/adobe-xd-50.svg'
+import photoshop from '../assets/icons/adobe-photoshop-50.svg'
+import illustrator from '../assets/icons/adobe-illustrator-50.svg'
+import figma from '../assets/icons/figma-50.svg'
+import word from '../assets/icons/word-50.svg'
+import excel from '../assets/icons/excel-50.svg'
+import jira from '../assets/icons/jira-50.svg'
+import notion from '../assets/icons/notion-50.svg'
 
 export default function skills( ) {
     return (
@@ -17,31 +35,45 @@ export default function skills( ) {
                             <div className="box">
                                 <h3>Programming</h3>
                                 <div className="skills-icons">
-                                    <img src={html} alt="HTML" width={35} height={35}></img>
+                                    <img src={html} title="HTML" alt="HTML" width={35} height={35}></img>
+                                    <img src={css} title="CSS" alt="CSS" width={35} height={35}></img>
+                                    <img src={js} title="Javascript" alt="Javascript" width={35} height={35}></img>
                                 </div>
                             </div>
                             <div className="box">
                                 <h3>Graphic Design</h3>
                                 <div className="skills-icons">
-                                   
+                                    <img src={photoshop} title="Photoshop" alt="Photoshop" width={35} height={35}></img>
+                                    <img src={illustrator} title="Illustrator" alt="Illustrator" width={35} height={35}></img>
+                                    <img src={xd} title="Adobe XD" alt="Adobe XD" width={35} height={35}></img>
+                                    <img src={figma} title="Figma" alt="Figma" width={35} height={35}></img>
                                 </div>
                             </div>
                             <div className="box">
                                 <h3>Librairies / Frameworks</h3>
                                 <div className="skills-icons">
-                                   
+                                    <img src={react} title="React" alt="React" width={35} height={35}></img>
+                                    <img src={redux} title="Redux" alt="Redux" width={35} height={35}></img>
+                                    <img src={sass} title="Sass" alt="Sass" width={35} height={35}></img>
                                 </div>
                             </div>
                             <div className="box">
                                 <h3>Project Management</h3>
                                 <div className="skills-icons">
-                                    
+                                    <img src={word} title="Word" alt="Word" width={35} height={35}></img>
+                                    <img src={excel} title="Excel" alt="Excel" width={35} height={35}></img>
+                                    <img src={jira} title="Jira" alt="Jira" width={35} height={35}></img>
+                                    <img src={notion} title="Notion" alt="Notion" width={35} height={35}></img>
                                 </div>
                             </div>
                             <div className="box">
                                 <h3>Tools / Plateforms</h3>
                                 <div className="skills-icons">
-                                    
+                                    <img src={wordpress} title="Wordpress" alt="Wordpress" width={35} height={35}></img>
+                                    <img src={seo} title="SEO" alt="SEO" width={35} height={35}></img>
+                                    <img src={git} title="Git" alt="Git" width={35} height={35}></img>
+                                    <img src={vscode} title="Vs Code" alt="Vs Code" width={35} height={35}></img>
+                                    <img src={firebase} title="Firebase" alt="Firebase" width={35} height={35}></img>
                                 </div>
                             </div>
                         </div>
@@ -54,7 +86,7 @@ export default function skills( ) {
                         Analytical skills, versatility, 
                         rigour, 
                         organisation,
-                        listening, responsiveness, curiosity, 
+                        listening, empathy, responsiveness, curiosity, 
                         creativity, 
                         critical thinking, autonomy.
                         </p>
