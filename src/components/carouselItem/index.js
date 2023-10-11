091b08c56d56a3b44c5b7ae442c00bbf1b270e66
Judee ジ <115565273+JudeeFr@users.noreply.github.com>
2023-10-11 15:31:40 +0200

    mise en forme slider

--- src/components/carouselItem/index.js
+++ src/components/carouselItem/index.js
@@ -7,10 +7,14 @@ export default function CarouselItem({item, width}) {
     
     return (
 		<div className="carousel-item" style={{ width: width }}>
-      <div></div>
-      {/* <img className="carousel-img" src={item.icon.default} /> */}
-      <div className="carousel-item-text">{item.title}</div>
-	  <div className="carousel-item-text">{item.description}</div>
+    		<div className="carousel-img">
+				<img className='carousel-picture1' src={item.pictures} alt="projet" />
+			</div>
+			<div className="carousel-item-text">
+				<h1 className='carousel-item-title'>{item.title}</h1>
+				<h2 className="carousel-item-subtitle">{item.subtitle}</h2>
+				<p className="carousel-item-description">{item.description}</p>
+			</div>
     </div>
 	);
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import CarouselItem from '../components/carouselItem';
-// import arrowLeft from '../assets/img/arrowleft.png';
-// import arrowRight from '../assets/img/arrowright.png';
+import arrowLeft from '../assets/img/arrowleft.png';
+import arrowRight from '../assets/img/arrowright.png';
 import Girl from '../components/illustrations/projects/girl';
 import Firstcloud from '../components/illustrations/projects/firstcloud';
 import Secondcloud from '../components/illustrations/projects/secondcloud'
@@ -36,6 +36,10 @@ useEffect(()=>{
 		setShowModal(true)
 		getWebDev()
 	  }
+	const closeModal = (e) => {
+		e.preventDefault()
+		setShowModal(false)
+	} 
 //slider
 const [activeIndex, setActiveIndex] = useState(0);
 const updateIndex = (newIndex) => {
@@ -76,7 +80,8 @@ const updateIndex = (newIndex) => {
 				<aside className='modal' style={{ display: !showModal ? 'none' : '' }}>
 					<div className="js-stop-modal">
 						<div className="modal-wrapper  carousel">								
-							<svg className="btn-close icon" fill="#000000" width="40px" height="40px" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+							<svg className="btn-close icon" 
+							onClick={closeModal} fill="#000000" width="40px" height="40px" x="20" y="0" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
 							<path d="M10.0303 8.96965C9.73741 8.67676 9.26253 8.67676 8.96964 8.96965C8.67675 9.26255 8.67675 9.73742 8.96964 10.0303L10.9393 12L8.96966 13.9697C8.67677 14.2625 8.67677 14.7374 8.96966 15.0303C9.26255 15.3232 9.73743 15.3232 10.0303 15.0303L12 13.0607L13.9696 15.0303C14.2625 15.3232 14.7374 15.3232 15.0303 15.0303C15.3232 14.7374 15.3232 14.2625 15.0303 13.9696L13.0606 12L15.0303 10.0303C15.3232 9.73744 15.3232 9.26257 15.0303 8.96968C14.7374 8.67678 14.2625 8.67678 13.9696 8.96968L12 10.9393L10.0303 8.96965Z" fill="#1C274C"/>
 							<path fillRule="evenodd" clipRule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="#1C274C"/>
 							</svg>
@@ -94,44 +99,38 @@ const updateIndex = (newIndex) => {
 									</div>
 
 									<div className="carousel-buttons">
-										<button
-										className="button-arrow"
-										onClick={() => {
-											updateIndex(activeIndex - 1);
-										}}
-										>
-										<span className="material-symbols-outlined">arrow_back_ios</span>{" "}
-										</button>
+										
+											<span className="btn-arrow arrow-left"onClick={() => {
+												updateIndex(activeIndex - 1);
+											}}>
+												<img src={arrowLeft} alt="arrow left"/>
+											</span>{" "}
+											
+										
 										<div className="indicators">
 										{works.map((item, index) => {
 											return (
-											<button
-												className="indicator-buttons"
-												onClick={() => {
+												<span onClick={() => {
 												updateIndex(index);
 												}}
-											>
-												<span
 												className={`material-symbols-outlined ${
 													index === activeIndex
 													? "indicator-symbol-active"
-													: "indicator-symbol"
+													: "indicator-symbol-unactive"
 												}`}
 												>
-												radio_button_checked
+												radio_button_unchecked
 												</span>
-											</button>
 											);
 										})}
 										</div>
-										<button
-										className="button-arrow"
-										onClick={() => {
-											updateIndex(activeIndex + 1);
-										}}
-										>
-										<span className="material-symbols-outlined">arrow_forward_ios</span>
-										</button>
+										<span className="btn-arrow arrow-right" onClick={() => {
+												updateIndex(activeIndex + 1);
+											}}>
+												<img src={arrowRight} alt="arrow right"/>
+											</span>
+										
+										
 									</div>
 									</div>	
 																						
